4e1f065335c70890f53e35b62e1d6b0618fae77b
Roderick Menor <rodpajmen@gmail.com>
2018-08-09 21:38:16 -1000

    Finished Functions Assignment

--- exercises.py
+++ exercises.py
@@ -5,6 +5,8 @@
 #1 
 #Declare two variables box_1 and box_2 and assign any number value of your choosing. These two variables will be used to invoke functions #2 - #5.
 
+box_1 = 10
+box_2 = 5
 
 #2
 #Function - add
@@ -14,6 +16,12 @@
 
 #Print the sum variable to see your result.
 
+def add(num_1, num_2):
+    return num_1 + num_2
+add(box_1, box_2)
+
+sum = add(box_1, box_2)
+print(sum)
 
 #3
 #Function - subtract
@@ -22,6 +30,13 @@
 #This function will subtract two numbers and return the difference of these two numbers. Invoke the function and assign it to a variable named `difference`.
 
 #Print the difference variable to see your result.
+
+def subtract(num_1, num_2):
+    return num_1 - num_2
+subtract(box_1, box_2)
+
+difference = subtract(box_1, box_2)
+print(difference)
 
 
 #4
@@ -32,6 +47,12 @@
 
 #Print the product variable to see your result.
 
+def multiply(num_1, num_2):
+    return num_1 * num_2    
+multiply(box_1, box_2)
+
+product = multiply(box_1, box_2)
+print(product)
 
 #5
 #Function - divide
@@ -40,6 +61,12 @@
 
 #Print the quotient variable to see your result.
 
+def divide(num_1, num_2):
+    return num_1 / num_2
+divide(box_1, box_2)
+
+quotient = divide(box_1, box_2)
+print(quotient)
 
 #6
 #Function - check_sum
@@ -48,6 +75,12 @@
 
 #Print the da_diva variable to see your result.
 
+def check_sum(x):
+    return 'Mariah Carey has been married ' + str(x) + ' amount of times.'
+check_sum(sum)
+
+da_diva = check_sum(sum)
+print(da_diva)
 
 #7
 #Function - check_difference
@@ -57,6 +90,12 @@
 
 #Print the loving_it variable to see your result.
 
+def check_difference(x):
+        return 'Last night I dreamt that I ate ' + str(x) + ' Big Macs.'
+check_difference(difference)
+
+loving_it = check_difference(difference)
+print(loving_it)
 
 #8
 #Function - check_product
@@ -64,13 +103,23 @@
 
 #This function will multiply the values stored in the sum and product variables. Invoke this function and print your result.
 
+def check_product():
+    return sum * product
+check_product()
+
+print(check_product())
 
 #9
 #Function - check_quotient
 #Create a function named `check_quotient` which does not require any parameters.
 
-#This function will multiply the values stored in the product and quotient variables. Invoke this function and print your result.
+#This function will divide the values stored in the product and quotient variables. Invoke this function and print your result.
 
+def check_quotient():
+    return product / quotient
+check_quotient()
+
+print(check_quotient())
 
 #10
 #Function - create_full_name
@@ -78,7 +127,14 @@
 
 #This function will return a string which represents someone's full name. Invoke this function by passing in your first and last name into the function. Store the return value into a variable named `my_full_name`.
 
-#Print the my_full_name variable to see your result.
+#Print the my_full_name variable to see your result.\
+
+def create_full_name(first_name, last_name):
+    return first_name + ' ' + last_name
+create_full_name ('Roderick', 'Menor')
+my_full_name = create_full_name('Roderick', 'Menor')
+
+print(my_full_name)
 
 
 
